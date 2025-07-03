c9afe7364bfedc7dacbe2d9f39107423d07fcf5b
Abdullah Intisar <abdullahintisar12@gmail.com>
2025-07-03 12:32:25 +0500

    commit from laptop

--- frontend/src/components/pages/SignUp/SignUp.jsx
+++ frontend/src/components/pages/SignUp/SignUp.jsx
@@ -47,7 +47,9 @@ const SignUp = () => {
 
             <div>
               <label className="label p-2">
-                <span className="text-base label-text text-white ">Username</span>
+                <span className="text-base label-text text-white ">
+                  Username
+                </span>
               </label>
               <input
                 type="text"
@@ -62,7 +64,9 @@ const SignUp = () => {
 
             <div>
               <label className="label p-2">
-                <span className="text-base label-text text-white">Password</span>
+                <span className="text-base label-text text-white">
+                  Password
+                </span>
               </label>
               <input
                 type="password"
@@ -77,7 +81,9 @@ const SignUp = () => {
 
             <div>
               <label className="label p-2">
-                <span className="text-base label-text text-white">Confirm Password</span>
+                <span className="text-base label-text text-white">
+                  Confirm Password
+                </span>
               </label>
               <input
                 type="password"
